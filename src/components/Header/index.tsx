fc267f15fd7c145560066641f7dd11a7316fc062
leonunesbs <leonunesbs@gmail.com>
2021-05-10 08:49:50 -0300

    update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 // #region Global Imports
-import { Flex, Image, useDisclosure } from '@chakra-ui/react';
+import { Flex, Image, SlideFade, useDisclosure } from '@chakra-ui/react';
 import {
   CallToActionButton,
   HamburgerMenu,
@@ -30,32 +30,44 @@ export const Header: React.FunctionComponent<IHeader.IProps> = ({
             justify="space-between"
             maxW="1280px"
           >
-            <Flex
-              transition="width 0.2s, height 0.2s"
-              h={[`50px`, `60px`, `80px`, `100px`, `120px`]}
-              minW="100px"
-            >
+            <Flex>
               <Image
-                d={[`none`, `flex`]}
+                d={['none', 'flex']}
                 src="/images/logo-light-h.png"
                 objectFit="contain"
+                transition="width 0.5s, height 0.5s"
+                h={['70px', '90px', '120px']}
               />
-              <Image
-                d={[`flex`, `none`]}
-                src="/images/logo-light.png"
-                objectFit="contain"
-              />
+              {mobileNavDisclosure.isOpen ? (
+                <SlideFade in={mobileNavDisclosure.isOpen} reverse>
+                  <Image
+                    d={['flex', 'none']}
+                    maxW="75%"
+                    src="/images/logo-light-h.png"
+                    objectFit="contain"
+                    transition="width 0.5s, height 0.5s"
+                  />
+                </SlideFade>
+              ) : (
+                <Image
+                  d={['flex', 'none']}
+                  src="/images/logo-light.png"
+                  objectFit="contain"
+                  transition="width 0.5s, height 0.5s"
+                  h={['50px', '60px', '80px', '100px', '120px']}
+                />
+              )}
             </Flex>
             <Flex>
               <HeaderMenu />
             </Flex>
             <Flex>
               <CallToActionButton
-                d={[`none`, `none`, `none`, `none`, `flex`, `flex`]}
+                d={['none', 'none', 'none', 'none', 'flex', 'flex']}
               />
               <HamburgerMenu
                 disclosure={mobileNavDisclosure}
-                d={[`flex`, `flex`, `flex`, `flex`, `none`, `none`]}
+                d={['flex', 'flex', 'flex', 'flex', 'none', 'none']}
               />
             </Flex>
           </Flex>
