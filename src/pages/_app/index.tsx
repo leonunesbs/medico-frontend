ebbd92c2664acbbcb3294ff4116ad8ce43cb642a
Leonardo Nunes <leonunesbs@gmail.com>
2021-05-13 12:00:53 -0300

    feat: gtag

--- src/pages/_app/index.tsx
+++ src/pages/_app/index.tsx
@@ -1,12 +1,26 @@
 // import App from "next/app";
-import React from 'react'
+import React, { useEffect } from 'react'
 import { AppProps /* , AppContext */ } from 'next/app'
 
 // 1. import `ChakraProvider` component
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from '@/styles/theme'
+import { useRouter } from 'next/router'
+import * as gtag from '@/utils/gtag'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      const handleRouteChange = (url: URL) => {
+        gtag.pageview(url)
+      }
+      router.events.on('routeChangeComplete', handleRouteChange)
+      return () => {
+        router.events.off('routeChangeComplete', handleRouteChange)
+      }
+    }
+  }, [router.events])
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
