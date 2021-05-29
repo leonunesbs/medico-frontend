e2a18c24cdc929b4b0286daff9655ef33078bb08
Leonardo Nunes <leonunesbs@gmail.com>
2021-05-29 00:18:51 -0300

    fix: fix

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -1,8 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const client = new ApolloClient({
-  uri: !(process.env.NODE_ENV === 'production')
-    ? 'https://leonunesbs-backend.herokuapp.com/graphql/'
-    : 'http://localhost:8000/graphql/',
+  uri:
+    process.env.NODE_ENV === 'production'
+      ? 'https://leonunesbs-backend.herokuapp.com/graphql/'
+      : 'http://localhost:8000/graphql/',
   cache: new InMemoryCache()
 })
